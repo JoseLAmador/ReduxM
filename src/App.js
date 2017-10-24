b596709c55e39e9a6b79df5afddd4bf85f4a432b
JoseLAmador <josel.amador.s@gmail.com>
2017-10-23 22:44:52 -0500

    reduxx

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm'
+
+export default class App extends Component<{}> {
+    componentWillMount(){
+        const config = {
+          apiKey: "AIzaSyBrx420xJAuQM1KPmYJnAxrMIaDfcQa1vI",
+          authDomain: "eatfast-f5acf.firebaseapp.com",
+          databaseURL: "https://eatfast-f5acf.firebaseio.com",
+          projectId: "eatfast-f5acf",
+          storageBucket: "eatfast-f5acf.appspot.com",
+          messagingSenderId: "354773888658"
+        };
+        firebase.initializeApp(config);
+    }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+    );
+  }
+}
